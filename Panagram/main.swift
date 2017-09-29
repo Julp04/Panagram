af216449f0901efee15e5356a98321b4fbee57e8
Julian Panucci <julpanucci@gmail.com>
2017-09-28 21:22:39 -0400

    Initial Commit

---
+++ Panagram/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Panagram
+//
+//  Created by Panucci, Julian R on 9/28/17.
+//  Copyright © 2017 Panucci, Julian R. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
